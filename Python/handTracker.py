2d7d7c456898d8fd24b42ec89e344f7815e205fc
relained <sewon7@gmail.com>
2025-05-11 01:06:53 +0900

    손의 경우 매퍼 없이 그대로 반환환

--- Python/handTracker.py
+++ Python/handTracker.py
@@ -3,14 +3,16 @@ import mediapipe as mp
 from cvzone.HandTrackingModule import HandDetector
 
 class handTracker:
-    def __init__(self, camera=cv.VideoCapture(0)):
+    def __init__(self, camera=cv.VideoCapture(0), xyMax=(1920, 1080)):
         self.handDetector = HandDetector(maxHands=1, detectionCon=0.8)
         self.camera = camera
+        self.yMax = xyMax[1]
 
     def get_hand_position(self):
         """
         현재 손 추적 반환
-        y값 보정 필요! y = (height - y)
+        보정 후 나와서 그대로 데이터 보내면 됨 (DEPRECATED 내부 tracker.old 참조)
+        hand값은 단순 좌표가 아니기 때문에 억지로 늘리면 안됨
         """
         success, image = self.camera.read()
         if not success:
@@ -21,6 +23,6 @@ class handTracker:
             lmList = hands[0]['lmList']
             hand_position = []
             for x, y, z in lmList:
-                hand_position.extend([x, y, z])
+                hand_position.extend([x, self.yMax - y, z])
         
-        return hand_position, image # y 보정 필요
+        return hand_position, image
